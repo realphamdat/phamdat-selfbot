f9ca28530f3b3caf33f3ef5d7d4c7b90f5a373c8
Phamdat <155445899+realphamdat@users.noreply.github.com>
2025-05-14 07:08:54 +0700

    Update log.py

--- modules/general/log.py
+++ modules/general/log.py
@@ -32,7 +32,7 @@ class Log:
 		logger = logging.getLogger(f"[{name.upper()}] - {str(self.client.user.name)}")
 		logger.setLevel(logging.DEBUG)
 		console_handler = logging.StreamHandler()
-		console_handler.setFormatter(CustomFormatter(is_file = color_console))
+		console_handler.setFormatter(CustomFormatter(is_file = not(color_console)))
 		logger.addHandler(console_handler)
 		if file_mode:
 			file = f"{file_directory}{str(self.client.user.name)}.txt"
