31649f778f64cfe40b53f308321e294629a49fcc
Phamdat <155445899+realphamdat@users.noreply.github.com>
2026-08-06 20:49:19 +0700

    Add files via upload

--- modules/bots/owo/boss.py
+++ modules/bots/owo/boss.py
@@ -15,30 +15,35 @@ class Boss:
         if not client.can_run():
             return
 
+        has_boss_appeared = False
+        fight_button = None
+
         for child in iter_children(message):
             content = section_content(child)
-            if not content or 'A Guild Boss Appeared!' not in content:
-                continue
+            if content and 'A Guild Boss Appeared!' in content:
+                has_boss_appeared = True
 
-            button = getattr(child, 'accessory', None)
-            if not button or getattr(button, 'custom_id', '') != 'guildboss_fight':
-                continue
-            if getattr(button, 'disabled', False):
-                return
+            accessory = getattr(child, 'accessory', None)
+            if accessory and getattr(accessory, 'custom_id', '') == 'guildboss_fight':
+                fight_button = accessory
+                if getattr(fight_button, 'disabled', False):
+                    return
 
-            channel = message.channel
-            watch_task = asyncio.create_task(Boss._watch_ticket_response(client, channel))
-            try:
-                await button.click()
-            except discord.HTTPException:
-                watch_task.cancel()
-                await asyncio.gather(watch_task, return_exceptions=True)
-                client.logger.exception('Failed to click boss fight')
-                return
+        if not has_boss_appeared or not fight_button:
+            return
 
-            client.logger.info(f'Joined boss battle in #{channel}')
-            await watch_task
-            break
+        channel = message.channel
+        watch_task = asyncio.create_task(Boss._watch_ticket_response(client, channel))
+        try:
+            await fight_button.click()
+        except discord.HTTPException:
+            watch_task.cancel()
+            await asyncio.gather(watch_task, return_exceptions=True)
+            client.logger.exception('Failed to click boss fight')
+            return
+
+        client.logger.info(f'Joined boss battle in #{channel}')
+        await watch_task
 
     @staticmethod
     async def _watch_ticket_response(client, channel):
