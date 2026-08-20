a846651c8e8e8b2753db5b16ebe24a8e577bb09e
Phamdat <155445899+realphamdat@users.noreply.github.com>
2026-08-20 18:39:28 +0700

    Update headless option for Chromium browser

--- modules/utils/topgg.py
+++ modules/utils/topgg.py
@@ -67,7 +67,7 @@ def _vote(bot_id, token):
     options = ChromiumOptions()
     options.set_browser_path(path)
     options.auto_port()
-    options.headless(True) 
+    options.set_argument('--headless=new')
     options.set_argument('--no-sandbox')
     options.set_argument('--disable-dev-shm-usage')
     options.set_argument('--disable-gpu')
